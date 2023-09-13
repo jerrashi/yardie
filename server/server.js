43ba773fbfc28ed9a63ca6dda77e13356962e590
Jerry Shi <40506087+jerrashi@users.noreply.github.com>
2023-09-13 11:48:23 -0400

    made an endpoint to retrieve the gift data

--- server/server.js
+++ server/server.js
@@ -18,3 +18,8 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`🚀 Server listening on http://localhost:${PORT}`)
 })
+
+
+import giftsRouter from './routes/gifts.js'
+
+app.use('/gifts', giftsRouter)
